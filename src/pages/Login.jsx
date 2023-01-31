702a0de34c1a60cb55327f990e7b20436cd1c77d
houssinCarnelian <houssin.carnelian@gmail.com>
2023-01-31 17:58:04 +0100

    make the navigation between login and register

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
-import { ToastContainer , toast} from 'react-toastify';
+import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 export default function Login() {
@@ -19,7 +20,7 @@ export default function Login() {
 
         <section className="h-screen">
             <div className="container px-6 py-12 h-full">
-                <ToastContainer  closeOnClick pauseOnFocusLoss pauseOnHover/>
+                <ToastContainer closeOnClick pauseOnFocusLoss pauseOnHover />
                 <div className="flex justify-center items-center flex-wrap h-full g-6 text-gray-800">
                     <div className="md:w-8/12 lg:w-6/12 mb-12 md:mb-0">
                         <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg" className="w-full" alt="Phone image" />
@@ -45,6 +46,7 @@ export default function Login() {
                             <button type="submit" className="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out w-full" data-mdb-ripple="true" data-mdb-ripple-color="light">
                                 Sign in
                             </button>
+                            <Link to='/register'><p className='text-center my-3'>I don't have an account?</p></Link>
                             <div className="flex items-center my-4 before:flex-1 before:border-t before:border-gray-300 before:mt-0.5 after:flex-1 after:border-t after:border-gray-300 after:mt-0.5">
                                 <p className="text-center font-semibold mx-4 mb-0">OR</p>
                             </div>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Register() {
   return (
@@ -53,6 +54,7 @@ export default function Register() {
                   <button className="block w-full max-w-xs mx-auto bg-indigo-500 hover:bg-indigo-700 focus:bg-indigo-700 text-white rounded-lg px-3 py-3 font-semibold">REGISTER NOW</button>
                 </div>
               </div>
+               <Link to='/login'><p className='text-center'>Already have an account?</p></Link> 
             </div>
           </div>
         </div>
