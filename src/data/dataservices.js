9bfe63e8203df9f842034002604634877c2f3bf0
houssinCarnelian <houssin.carnelian@gmail.com>
2023-02-05 17:37:26 +0100

    fixing image for deployment isue of public

--- src/data/dataservices.js
+++ src/data/dataservices.js
@@ -2,25 +2,25 @@ export const data =
     [
         {
             id:1,
-            img: '../../public/assets/shipped.png',
+            img: 'assets/shipped.png',
             title: 'Free Shipping',
            
         },
         {
             id:2,
-            img: '../../public/assets/gift-box.png',
+            img: 'assets/gift-box.png',
             title: 'Gift Card',
            
         },
         {
             id:3,
-            img:'../../public/assets/return.png' ,
+            img:'assets/return.png' ,
             title: '7 days return',
            
         },
         {
             id:4,
-            img:'../../public/assets/send.png' ,
+            img:'assets/send.png' ,
             title: 'Contact us',
            
         },
